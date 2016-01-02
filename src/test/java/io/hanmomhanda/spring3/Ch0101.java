c8c218e0391673b45ab31da7744571f74bb84cc6
hanmomhanda <hanmomhanda@gmail.com>
2016-01-02 15:20:15 +0900

    1장 초난감 DAO

--- src/test/java/io/hanmomhanda/spring3/Ch0101.java
+++ src/test/java/io/hanmomhanda/spring3/Ch0101.java
@@ -1,33 +1,37 @@
 package io.hanmomhanda.spring3;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
 import io.hanmomhanda.spring3.ch01.dao.UserDao;
 import io.hanmomhanda.spring3.ch01.domain.User;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.util.Arrays;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Ch0101.class)
 public class Ch0101 {
 
+	UserDao userDao;
+    String userId;
+
+	@Before
+	public void before() throws Exception {
+		userDao = new UserDao();
+        userId = "hanmomhanda";
+        userDao.delete(userId);
+	}
+
 	@Test
 	public void addAndGet() throws Exception {
 		User user = new User();
-		user.setId("hanmomhanda");
+		user.setId(userId);
 		user.setName("오명운");
 		user.setPassword("2369home");
-
-        UserDao userDao = new UserDao();
-
-        userDao.delete("hanmomhanda");
 
         userDao.add(user);
 
@@ -36,4 +40,9 @@ public class Ch0101 {
         assertThat(userFromDB.getName(), is(user.getName()));
         assertThat(userFromDB.getPassword(), is(user.getPassword()));
 	}
+
+    @After
+    public void after() throws Exception {
+        userDao.delete(userId);
+    }
 }

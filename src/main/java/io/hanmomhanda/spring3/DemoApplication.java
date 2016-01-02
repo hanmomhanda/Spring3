9795327a0405e9625bc9adf28de08782276afe7d
hanmomhanda <hanmomhanda@gmail.com>
2016-01-02 15:20:15 +0900

    1장 초난감 DAO

--- src/main/java/io/hanmomhanda/spring3/DemoApplication.java
+++ src/main/java/io/hanmomhanda/spring3/DemoApplication.java
@@ -1,5 +1,7 @@
 package io.hanmomhanda.spring3;
 
+import io.hanmomhanda.spring3.ch01.dao.UserDao;
+import io.hanmomhanda.spring3.ch01.domain.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,14 +11,30 @@ import java.util.Arrays;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
-		ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
+		UserDao userDao = new UserDao();
 
-		String[] beanNames = ctx.getBeanDefinitionNames();
-		Arrays.sort(beanNames);
-		for (String beanName : beanNames) {
-			System.out.println(beanName);
-		}
+		String userId = "hanmomhanda";
+		userDao.delete(userId);
+
+		User user = new User();
+		user.setId(userId);
+		user.setName("오명운");
+		user.setPassword("2369home");
+
+		userDao.add(user);
+
+		User userFromDB = userDao.get(user.getId());
+
+		System.out.println(user.getName());
+		System.out.println(userFromDB.getName());
+		System.out.println("=== " + user.getName().equals(userFromDB.getName()) + " ===\n");
+
+		System.out.println(user.getPassword());
+		System.out.println(userFromDB.getPassword());
+		System.out.println("=== " + user.getPassword().equals(userFromDB.getPassword()) + " ===\n");
+
+		userDao.delete(userId);
 	}
 }

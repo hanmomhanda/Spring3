5a97fe4578079176a67f12c014d6e1ef1cdcbf3c
hanmomhanda <hanmomhanda@gmail.com>
2016-01-03 15:17:37 +0900

    Factory에서 ConnectionMakerImpl 주입하도록 수정

--- src/main/java/io/hanmomhanda/spring3/DemoApplication.java
+++ src/main/java/io/hanmomhanda/spring3/DemoApplication.java
@@ -12,7 +12,6 @@ public class DemoApplication {
 	public static void main(String[] args) throws Exception {
 
 		UserDao userDao = DaoFactory.getUserDao();
-		userDao.setConnectionMaker(new ConnectionMakerImpl());
 
 		String userId = "hanmomhanda";
 		userDao.delete(userId);
--- src/main/java/io/hanmomhanda/spring3/ch01/dao/DaoFactory.java
+++ src/main/java/io/hanmomhanda/spring3/ch01/dao/DaoFactory.java
@@ -7,6 +7,7 @@ public class DaoFactory {
     private static UserDao userDao = new UserDao();
 
     public static UserDao getUserDao() {
+        userDao.setConnectionMaker(new ConnectionMakerImpl());
         return userDao;
     }
 }

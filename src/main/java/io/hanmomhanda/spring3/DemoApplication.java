a4160cbcd651ca86fab1217e06697278cb602a48
hanmomhanda <hanmomhanda@gmail.com>
2016-01-03 23:18:42 +0900

    테스트코드를 JUnit 테스트로 이동

--- src/main/java/io/hanmomhanda/spring3/DemoApplication.java
+++ src/main/java/io/hanmomhanda/spring3/DemoApplication.java
@@ -1,38 +1,50 @@
 package io.hanmomhanda.spring3;
 
-import io.hanmomhanda.spring3.ch03.dao.ConnectionMakerImpl;
 import io.hanmomhanda.spring3.ch03.dao.DaoFactory;
 import io.hanmomhanda.spring3.ch03.dao.UserDao;
 import io.hanmomhanda.spring3.ch03.domain.User;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) throws Exception {
 
-		UserDao userDao = DaoFactory.getUserDao();
+		ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
 
-		String userId = "hanmomhanda";
-		userDao.delete(userId);
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for (String beanName : beanNames) {
+			System.out.println(beanName);
+		}
 
-		User user = new User();
-		user.setId(userId);
-		user.setName("오명운");
-		user.setPassword("2369home");
-
-		userDao.add(user);
-
-		User userFromDB = userDao.get(user.getId());
-
-		System.out.println(user.getName());
-		System.out.println(userFromDB.getName());
-		System.out.println("=== " + user.getName().equals(userFromDB.getName()) + " ===\n");
-
-		System.out.println(user.getPassword());
-		System.out.println(userFromDB.getPassword());
-		System.out.println("=== " + user.getPassword().equals(userFromDB.getPassword()) + " ===\n");
-
-		userDao.delete(userId);
+		// JUnit 테스트로 이동
+//		UserDao userDao = DaoFactory.getUserDao();
+//
+//		String userId = "hanmomhanda";
+//		userDao.delete(userId);
+//
+//		User user = new User();
+//		user.setId(userId);
+//		user.setName("오명운");
+//		user.setPassword("2369home");
+//
+//		userDao.add(user);
+//
+//		User userFromDB = userDao.get(user.getId());
+//
+//		System.out.println(user.getName());
+//		System.out.println(userFromDB.getName());
+//		System.out.println("=== " + user.getName().equals(userFromDB.getName()) + " ===\n");
+//
+//		System.out.println(user.getPassword());
+//		System.out.println(userFromDB.getPassword());
+//		System.out.println("=== " + user.getPassword().equals(userFromDB.getPassword()) + " ===\n");
+//
+//		userDao.delete(userId);
 	}
 }
--- src/test/java/io/hanmomhanda/spring3/DemoApplicationTests.java
+++ src/test/java/io/hanmomhanda/spring3/DemoApplicationTests.java
@@ -1,23 +1,49 @@
 package io.hanmomhanda.spring3;
 
+import io.hanmomhanda.spring3.ch03.dao.DaoFactory;
+import io.hanmomhanda.spring3.ch03.dao.UserDao;
+import io.hanmomhanda.spring3.ch03.domain.User;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DemoApplication.class)
 public class DemoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-//		ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
-//
-//		String[] beanNames = ctx.getBeanDefinitionNames();
-//		Arrays.sort(beanNames);
-//		for (String beanName : beanNames) {
-//			System.out.println(beanName);
-//		}
+	UserDao userDao;
+	String userId;
+
+	@Before
+	public void before() throws Exception {
+		userDao = DaoFactory.getUserDao();
+		userId = "hanmomhanda";
+		userDao.delete(userId);
 	}
 
+	@Test
+	public void addAndGet() throws Exception {
+		User user = new User();
+		user.setId(userId);
+		user.setName("오명운");
+		user.setPassword("2369home");
+
+		userDao.add(user);
+
+		User userFromDB = userDao.get(user.getId());
+
+		assertThat(userFromDB.getName(), is(user.getName()));
+		assertThat(userFromDB.getPassword(), is(user.getPassword()));
+	}
+
+	@After
+	public void after() throws Exception {
+		userDao.delete(userId);
+	}
 }
